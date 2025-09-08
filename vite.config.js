da602934e223116895714b6f4d42848759ce266c
Delunsss2345 <huypham1459@gmail.com>
2025-09-08 17:33:43 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: command === "build" ? "/f8-zoom-day-36/" : "/",
+  base: command === "build" ? "/f8-zoom-day36/" : "/",
 }));
